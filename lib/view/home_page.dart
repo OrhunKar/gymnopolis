bdb485d5ba5bcceccc24439793a1ea69cc3d4e98
canavci312 <canavci95@gmail.com>
2019-02-15 17:08:43 +0300

    Navigation bar working but cant navigate to new page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,52 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:gymnopolis/model/Workout.dart';
 import 'package:gymnopolis/view/day_page.dart';
-
-class HomePage extends StatelessWidget {
+import 'package:gymnopolis/control/placeholder_widget.dart';
+class HomePage extends StatefulWidget {
   static String tag = 'home-page';
 
+  createState() {
+    return HomePageState();
+  }
+}
   final List<Workout> workouts = Workout.allWorkouts();
 
   @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Workout Plan'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.edit), onPressed: null)
-        ],
-      ),
-      body: new WorkoutList(workouts), //List displayed here
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed, //This prevents type to change to shifting
-        currentIndex: 0, // this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.directions_bike),
-            title: new Text('Workout'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.local_dining),
-            title: new Text('Nutrition'),
-          ),
-          BottomNavigationBarItem(
+  class HomePageState extends State<HomePage> {
+    int _currentIndex = 0;
+    final List<Widget> _children = [   PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green)];
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Workout Plan'),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.edit), onPressed: null)
+          ],
+        ),
+        body: new WorkoutList(workouts), //List displayed here
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          type: BottomNavigationBarType.fixed,
+          //This prevents type to change to shifting
+          currentIndex: _currentIndex,
+          // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.directions_bike),
+              title: new Text('Workout'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.local_dining),
+              title: new Text('Nutrition'),
+            ),
+            BottomNavigationBarItem(
               icon: new Icon(Icons.message),
               title: new Text('Messages'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.school),
-            title: new Text('Trainers'),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: new Text('Profile')
-          )
-        ],
-      ),
-    );
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.school),
+              title: new Text('Trainers'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: new Text('Profile')
+            )
+          ],
+        ),
+      );
+    }
+    void onTabTapped(int index) {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
   }
-}
 
 class WorkoutList extends StatelessWidget {
 
