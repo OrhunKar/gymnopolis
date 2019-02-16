1f8eaf24788a43e7d399e4964aa144f407d15587
canavci312 <canavci95@gmail.com>
2019-02-16 21:20:13 +0300

    nutrition page

--- lib/view/nutrition_page.dart
+++ lib/view/nutrition_page.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:gymnopolis/model/Workout.dart';
 import 'package:gymnopolis/view/day_page.dart';
 import 'package:gymnopolis/control/placeholder_widget.dart';
+import 'package:zoomable_image/zoomable_image.dart';
 class NutritionPage extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Container(
-      height: 300.0,
-      color: Colors.purple,
+    return new Scaffold(
+      body: new ZoomableImage(
+          new AssetImage('assets/beslenme.png'),
+          placeholder: const Center(child: const CircularProgressIndicator()),
+          backgroundColor: Colors.transparent),
     );
   }
 }
