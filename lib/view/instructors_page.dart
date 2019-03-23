996ab5e0580a67c20913861e0e60863a831d1d36
canavci312 <canavci95@gmail.com>
2019-03-23 17:13:23 +0300

    instructors page

--- lib/view/instructors_page.dart
+++ lib/view/instructors_page.dart
@@ -1,44 +1,51 @@
+//Log In page design inspired from https://github.com/putraxor/flutter-login-ui
+
 import 'package:flutter/material.dart';
-import 'package:gymnopolis/model/Workout.dart';
-import 'package:gymnopolis/view/day_page.dart';
-import 'package:gymnopolis/control/placeholder_widget.dart';
-import 'package:gymnopolis/view/nutrition_page.dart';
-final List<Workout> workouts = Workout.allWorkouts();
-class InstructorsPage extends StatefulWidget {
-  static String tag = 'instructors-page';
-  createState() {
-    return InstructorsState(workouts);
-  }
+import 'package:gymnopolis/view/home_page.dart';
 
-}
+class InstructorsPage extends StatelessWidget {
 
-
-class InstructorsState extends State<InstructorsPage> {
-  final List<Workout> _workouts;
-  InstructorsState(this._workouts);
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+                "Trainers available at the moment:"
+            ),
+            SizedBox(height: 10.0,),
+            InstructorCard()
+          ],
+        )
+    );
+  }
 
-    return new ListView.builder(
-      itemCount: _workouts.length,
-      itemBuilder: (BuildContext context, int index){
-        return new GestureDetector(
-          onTap: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DayPage()),
-            );},
-          child: new ListTile(
-              title : new Text(_workouts[index].name),
-              subtitle: new Text(_workouts[index].instructorName + " Hoca"),
-              leading: new CircleAvatar(
-                  child: new Text((_workouts[index].day).toString())
-              )
+}
+class InstructorCard extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 100.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10.0),),
+            border: Border.all(color: Colors.deepOrange),
+
           ),
-        ) ;
-      },
-      padding: new EdgeInsets.symmetric(vertical: 8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text("sa")
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/view/messages_page.dart
+++ lib/view/messages_page.dart
@@ -13,90 +13,93 @@ class MessagesPageState extends State<MessagesPage> {
   Widget build(BuildContext context) {
     int _radioValue1 = -1;
     return Form(
-      key: _formKey,
+
+     key: _formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            initialValue: "canavci95@gmail.com",
-            decoration: const InputDecoration(
-              labelText: 'Email',
-            ),
+      crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+    TextFormField(
+    initialValue: "canavci95@gmail.com",
+    decoration: const InputDecoration(
+    labelText: 'Email',
+    ),
 
-          ),
-          const SizedBox(height: 16.0),
-          TextFormField(
-            initialValue: "Can Avcı",
-            decoration: const InputDecoration(
-              labelText: 'Full name',
-            ),
+    ),
+    const SizedBox(height: 16.0),
+    TextFormField(
+    initialValue: "Can Avcı",
+    decoration: const InputDecoration(
+    labelText: 'Full name',
+    ),
 
-          ),
-          const SizedBox(height: 16.0),
-          TextFormField(
-            initialValue: "1.78",
-            decoration: const InputDecoration(
-              labelText: 'Height',
-            ),
+    ),
+    const SizedBox(height: 16.0),
+    TextFormField(
+    initialValue: "1.78",
+    decoration: const InputDecoration(
+    labelText: 'Height',
+    ),
 
-          ),
-          const SizedBox(height: 16.0),
-          TextFormField(
-            initialValue: "70",
-            decoration: const InputDecoration(
-              labelText: 'Weight',
-            ),
+    ),
+    const SizedBox(height: 16.0),
+    TextFormField(
+    initialValue: "70",
+    decoration: const InputDecoration(
+    labelText: 'Weight',
+    ),
 
-          ),
-          const SizedBox(height: 16.0),
-          TextFormField(
-            initialValue: "5323911251",
-            decoration: const InputDecoration(
-              labelText: 'Phone',
-            ),
+    ),
+    const SizedBox(height: 16.0),
+    TextFormField(
+    initialValue: "5323911251",
+    decoration: const InputDecoration(
+    labelText: 'Phone',
+    ),
 
-          ),
-          const SizedBox(height: 16.0),
-          new Text(
-            'Gender:',
-            style: new TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
-            ),
-          ),
-          new Text(
-            'Male',
-            style: new TextStyle(fontSize: 16.0),
-          ),
-          new Radio(
-            value: 0,
-            groupValue: _radioValue1,
+    ),
+    const SizedBox(height: 16.0),
+    new Text(
+    'Gender:',
+    style: new TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18.0,
+    ),
+    ),
+    new Text(
+    'Male',
+    style: new TextStyle(fontSize: 16.0),
+    ),
+    new Radio(
+    value: 0,
+    groupValue: _radioValue1,
 
-          ),
-          new Text(
-            'Female',
-            style: new TextStyle(fontSize: 16.0),
-          ),
+    ),
+    new Text(
+    'Female',
+    style: new TextStyle(fontSize: 16.0),
+    ),
 
 
-          new Radio(
-            value: 1,
-            groupValue: _radioValue1,
+    new Radio(
+    value: 1,
+    groupValue: _radioValue1,
 
-          ),
+    ),
 
-          Row(
-            children: <Widget>[
-              const Spacer(),
-              OutlineButton(
-                  highlightedBorderColor: Colors.black,
-                  //        onPressed: _submittable() ? _submit : null,
-                  child: const Text('Save Changes')
-              ),
-            ],
-          ),
-        ],
-      ),
+    Row(
+    children: <Widget>[
+    const Spacer(),
+    OutlineButton(
+    highlightedBorderColor: Colors.black,
+    //        onPressed: _submittable() ? _submit : null,
+    child: const Text('Save Changes')
+    ),
+    ],
+    ),
+    ],
+    ),
+
     );
+
   }
 }
