84f4be992a462cdfa2dc825b90db4df84089e225
canavci312 <canavci95@gmail.com>
2019-03-23 18:56:31 +0300

    instructors pagev2

--- lib/view/instructors_page.dart
+++ lib/view/instructors_page.dart
@@ -29,23 +29,66 @@ class InstructorCard extends StatelessWidget{
     return Stack(
       children: <Widget>[
         Container(
-          height: 100.0,
+          margin: const EdgeInsets.only(right: 16.0),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(10.0),),
-            border: Border.all(color: Colors.deepOrange),
+            border: Border.all(color: Colors.blueGrey),
 
           ),
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text("sa")
-                ],
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+
+                  children: <Widget>[
+                    Container(
+                        width: 60.0,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: new DecorationImage(
+                                fit: BoxFit.fill,
+                                image: new NetworkImage(
+                                    "https://i.hizliresim.com/k9dbRJ.png")
+                            )
+
+                        ),
+
+                    ),
+                    new Text("Ayca_22"),
+                  ],
+
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: <Widget>[
+                    InstructorDetail(Icons.account_balance_wallet, '70\$ month'),
+                    InstructorDetail(Icons.star, '4.4')
+                  ],
+
+                )
+              ],
+            ),
           ),
         ),
       ],
+    );
+  }
+}
+class InstructorDetail extends StatelessWidget{
+  final IconData iconData;
+  final String label;
+  InstructorDetail(this.iconData, this.label);
+
+  Widget build(BuildContext context){
+    return RawChip(
+      label: Text(label),
+      labelStyle: TextStyle(color: Colors.black, fontSize: 14.0),
+      backgroundColor: Colors.white,
+      avatar: Icon(iconData, size: 14.0),
     );
   }
 }
