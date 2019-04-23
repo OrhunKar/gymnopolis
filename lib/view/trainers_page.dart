09ba1707358ab1073b1a04dc4db5809ef500d751
canavci312 <canavci95@gmail.com>
2019-04-23 20:22:28 +0300

    instructors page problem

---
+++ lib/view/trainers_page.dart
@@ -0,0 +1,33 @@
+//Log In page design inspired from https://github.com/putraxor/flutter-login-ui
+
+import 'package:flutter/material.dart';
+import 'package:gymnopolis/view/home_page.dart';
+
+class TrainersPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Trainers available at the moment:"
+          ),
+          SizedBox(height: 10.0,),
+        ],
+      )
+    );
+  }
+
+}
+class TrainerCard extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      height: 100.0,
+    );
+  }
+}
