f5ec02a5121244418b6e59d861270b51e568d260
OrhunKar <orhun.kar.111@gmail.com>
2019-04-30 13:57:38 +0300

    Student changes last

--- lib/view/instructor/students_page.dart
+++ lib/view/instructor/students_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gymnopolis/model/InstructorModels/Trainee.dart';
 import 'package:gymnopolis/view/Page.dart';
+import 'package:gymnopolis/view/instructor/studentPages/student_profile_page.dart';
 
 final List<Trainee> _trainees = Trainee.allTrainees();
 
@@ -8,7 +9,14 @@ class StudentsPage extends StatefulWidget with Page{
   static String tag = 'student-page';
 
   List<Widget> topBar(BuildContext context){
-    return null;
+    return <Widget>[
+      new IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () {
+          showSearch(context: context, delegate: TraineeSearch());
+        },
+      ),
+    ];
   }
 
   createState() {
@@ -18,14 +26,63 @@ class StudentsPage extends StatefulWidget with Page{
 
 @override
 class StudentsPageState extends State<StudentsPage> {
-  String defaultSearch = 'Name Search';
-  Icon actionIcon = new Icon(Icons.search);
-  Widget appBarTitle = new Text("Students");
-
 
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemCount: _trainees.length,
+    return HelperWidget.pageContent(context, _trainees);
+  }
+}
+
+//Search class
+class TraineeSearch extends SearchDelegate<Trainee>{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(icon: Icon(Icons.clear),
+        onPressed: (){
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: (){
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    final results = _trainees.where((a) => a.name.toLowerCase().contains(query));
+
+    return HelperWidget.pageContent(context, results);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    final results = _trainees.where((a) => a.name.toLowerCase().contains(query));
+
+    return HelperWidget.pageContent(context, results);
+
+  }
+
+}
+
+
+//Helper Widget class
+class HelperWidget{
+
+  static Widget pageContent(BuildContext context, List<Trainee> results){
+    return ListView.builder(
+      itemCount: results.length,
       itemBuilder: (BuildContext context, int index){
         return new GestureDetector(
           onTap: (){
@@ -34,8 +91,14 @@ class StudentsPageState extends State<StudentsPage> {
               MaterialPageRoute(builder: (context) => DayPage()),
             );*/},
           child: new ListTile(
-              title : new Text(_trainees[index].name),
-              subtitle: new Text(_trainees[index].lastActivity.toString()),
+            title : new Text(results.toList()[index].name),
+            subtitle: new Text(results.toList()[index].lastActivity.toString() + ' minutes ago'),
+            onTap: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => StudentProfilePage(results.toList()[index]),
+                  )
+              );},
           ),
         ) ;
       },
